62a3727cc3b48829853630686d0828550cb41024
bytedriver <hi@bytedriver.me>
2023-04-05 16:01:25 -0400

    Creating and Combining Views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by admin on 4/5/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by admin on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
