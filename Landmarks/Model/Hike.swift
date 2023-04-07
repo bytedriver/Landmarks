032f0e43b6f36b57fe6cdedebcee7183e9d4be78
bytedriver <hi@bytedriver.me>
2023-04-06 20:42:18 -0400

    Animating Views and Transitions

--- Landmarks/Model/Hike.swift
+++ Landmarks/Model/Hike.swift
@@ -1,5 +1,5 @@
 //
-//  ModelData.swift
+//  Hike.swift
 //  Landmarks
 //
 //  88                                                     88              88                                     
@@ -18,31 +18,26 @@
 //
 
 import Foundation
-import Combine
 
-final class ModelData: ObservableObject {
-    @Published var landmarks: [Landmark] = load("landmarkData.json")
-    var hikes: [Hike] = load("hikeData.json")
-}
-
-func load<T: Decodable>(_ filename: String) -> T {
-    let data: Data
+struct Hike: Codable, Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
     
-    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
     }
     
-    do {
-        data = try Data(contentsOf: file)
-    } catch {
-        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
-    }
-    
-    do {
-        let decoder = JSONDecoder()
-        return try decoder.decode(T.self, from: data)
-    } catch {
-        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+        
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
     }
 }
